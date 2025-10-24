48f7fa34896b2a813ca63d69d7bab7a66fddc582
Churakov Ivan <IChurakov@alrud.com>
2025-10-24 17:26:38 +0300

    add test Track

--- jest.config.js
+++ jest.config.js
@@ -4,8 +4,24 @@ const tsJestTransformCfg = createDefaultPreset().transform;
 
 /** @type {import("jest").Config} **/
 module.exports = {
-  testEnvironment: "node",
+  preset: "ts-jest",
+  testEnvironment: "jsdom", // 👈 нужно для React
   transform: {
     ...tsJestTransformCfg,
+  },
+  globals: {
+    "ts-jest": {
+      tsconfig: {
+        jsx: "react-jsx", // 👈 важно! позволяет парсить JSX в TSX
+      },
+    },
+  },
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"], // подключаем jest-dom
+  moduleNameMapper: {
+    // игнорируем импорты css, изображений и т.д.
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    "^.+\\.(png|jpg|jpeg|gif|svg)$": "<rootDir>/__mocks__/fileMock.js",
+    "^next/link$": "next/dist/client/link.js",
+    "^next/router$": "next/dist/client/router.js"
   },
 };
